ae5e675184ac69477046ec6d75fd234851e621c4
Nelly Sankova <nellysank@yandex.ru>
2025-04-05 14:26:36 +0300

    Скрипт отображения всех карточек и удаления одной карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const listCard = document.querySelector('.places__list')
 
 // @todo: Функция создания карточки
+const AddCard = (cardName, cardLink, deleteCard) => {
+  const newCard = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  newCard.querySelector('.card__title').textContent = cardName;
+  newCard.querySelector('.card__image').src = cardLink;
+
+  const deleteButton = newCard.querySelector('.card__delete-button');
+
+  deleteButton.addEventListener('click', function (evt) {
+    deleteCard(evt.target);
+  });
+
+  return newCard;
+}
 
 // @todo: Функция удаления карточки
+const deleteCard = (buttonItem) => {
+  const ParentItem = buttonItem.closest('.places__item');
+  ParentItem.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(
+  function (item) {
+    const itemCard = AddCard(item.name, item.link, deleteCard);
+    listCard.append(itemCard);
+  });
