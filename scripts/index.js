6bb1eeb451568095b409c1eac54143a87a4e880a
Nelly Sankova <nellysank@yandex.ru>
2025-04-05 15:30:38 +0300

    Доработки по ревью: переименование метода, camelCase в наименовании переменных, добавление alt, изменение входного параметра deleteCard

--- scripts/index.js
+++ scripts/index.js
@@ -5,29 +5,27 @@ const cardTemplate = document.querySelector('#card-template').content;
 const listCard = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-const AddCard = (cardName, cardLink, deleteCard) => {
+const createCard = (cardName, cardLink, deleteCard) => {
   const newCard = cardTemplate.querySelector('.places__item').cloneNode(true);
 
   newCard.querySelector('.card__title').textContent = cardName;
   newCard.querySelector('.card__image').src = cardLink;
+  newCard.querySelector('.card__image').alt = `Фотография места: ${cardName}`;
 
   const deleteButton = newCard.querySelector('.card__delete-button');
 
-  deleteButton.addEventListener('click', function (evt) {
-    deleteCard(evt.target);
-  });
+  deleteButton.addEventListener('click', () => deleteCard(newCard));
 
   return newCard;
 };
 
 // @todo: Функция удаления карточки
-const deleteCard = (buttonItem) => {
-  const ParentItem = buttonItem.closest('.places__item');
-  ParentItem.remove();
+const deleteCard = (cardItem) => {
+  cardItem.remove();
 };
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(function (item) {
-  const itemCard = AddCard(item.name, item.link, deleteCard);
+initialCards.forEach((item) => {
+  const itemCard = createCard(item.name, item.link, deleteCard);
   listCard.append(itemCard);
 });
